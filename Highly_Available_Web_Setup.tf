57c2c54a336e97d64497f3c4cc6bcab53b92a4ef
Dylan-MaCHINe <152349545+Dylan-MaCHINe@users.noreply.github.com>
2023-11-30 08:50:37 -0800

    Update Highly_Available_Web_Setup.tf

--- Highly_Available_Web_Setup.tf
+++ Highly_Available_Web_Setup.tf
@@ -61,3 +61,81 @@ resource "aws_security_group" "allow-http" {
     Name = "HTTP security group"
   }
 }
+
+# after, we'll create our EC2 instance to host a web server
+# note: your ami may be different than mine
+resource "aws_instance" "web server" {
+  ami                    = "ami-0fc5d935ebf8bc3bc"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.public-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.allow-http.id]
+  tags = {
+    Name = "Web Server"
+  }
+}
+
+# next we'll create a route table and define our route to the internet gateway 
+# and allow outbound internet access  
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.main-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main-gw.id
+  }
+}
+# then create 2 route table association resource which will ensure our public
+# subnets are associated with a route table, and that includes a route to the Internet Gateway
+resource "aws_route_table_association" "public_subnet_1_association" {
+  subnet_id      = aws_subnet.public-subnet-1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+resource "aws_route_table_association" "public_subnet_2_association" {
+  subnet_id      = aws_subnet.public-subnet-2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# after that, we'll create an application load balancer and target group
+# the application load balancer will evenly distribute traffic across multiple
+# targets described in the target group
+resource "aws_lb" "web_alb" {
+  name                       = "web_alb"
+  internal                   = false
+  load_balancer_type         = "application"
+  security_groups            = [aws_security_group.allow-http.id]
+  subnets                    = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+  enable_deletion_protection = false
+}
+resource "aws_lb_target_group" "web_tg" {
+  name     = "web_tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main-vpc.id
+}
+
+# moving on, we'll create an auto-scaling group to automatically adjust the number of EC2 instances
+resource "aws_autoscaling_group" "web_asg" {
+  launch_configuration = aws_launch_configuration.web_config.id
+  vpc_zone_identifier  = [aws_subnet.public-subnet-1.id, aws_subnet.public-subnet-2.id]
+  target_group_arns    = [aws_lb_target_group.web_tg.arn]
+  min_size             = 1
+  max_size             = 3
+  health_check_type    = "ELB"
+
+  tag {
+    key                 = "Name"
+    value               = "WebServerInstance"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_launch_configuration" "web_config" {
+  name          = "web-launch-config"
+  image_id      = "ami-0fc5d935ebf8bc3bc"
+  instance_type = "t2.micro"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
